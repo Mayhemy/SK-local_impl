a8c6db64fc25ce01acad769fcd37dfa1a272ac10
Mayhemy <58441981+Mayhemy@users.noreply.github.com>
2022-10-27 17:05:16 +0200

    test123

---
+++ src/main/java/Local_impl/LocalStorage.java
@@ -0,0 +1,13 @@
+package Local_impl;
+
+import Storage.StorageAndCfg.Cfg;
+import Storage.StorageAndCfg.StorageSpec;
+
+public class LocalStorage extends StorageSpec {
+    public LocalStorage() {
+        super();
+    }
+    void saveConfig(Cfg cfg){
+        return;
+    }
+}
